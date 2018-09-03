59ab1a65aa580a77e04670a3d28f6643e1779d80
Unknown <fydte@mail.ru>
2018-09-03 21:26:03 +0900

    added some comments

--- my_capstone.py
+++ my_capstone.py
@@ -17,9 +17,23 @@ def show_ldf_nums(distance, amplitude):
     for num in range(len(distance)):
         print(f'{distance[num]} {amplitude[num]}')   
 
+def calculate_depth_one(ratio):
+    result = 655 + 100*(ratio - 3.01)/0.46
+    return result
+
+def calculate_depth_two(ratio):
+    return 660 + 100(ratio-11.5)/3
+
+def MNK(amp, dist):
+#there should be an algorithm of MNK to get coefficients
+#maybe it should return a list? or only coefficients?
+    pass
+
+
 distance = []
 amplitude = []
 
 unfold('C:\\python\\input_files\\input_ldf_01.txt')
 
 show_ldf_nums(distance, amplitude)
+
